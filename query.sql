1d274865fe38b8f3fa66dd57828cbe76b52ac877
Jeff <yfsuse@gmail.com>
2014-11-22 14:14:03 +0800

    add

---
+++ query.sql
@@ -0,0 +1,8 @@
+select conv_time,
+offer_id,
+MAX(CASE status WHEN 'Confirmed' THEN countstatus ELSE 0 END ) Confirmed,
+MAX(CASE status WHEN 'Accepted' THEN countstatus ELSE 0 END ) Accepted,
+(MAX(CASE status WHEN 'Confirmed' THEN countstatus ELSE 0 END )-MAX(CASE status WHEN 'Accepted' THEN countstatus ELSE 0 END)) diff,
+url from
+(select conv_time,offer_id, status, count(status) as countstatus, url from statustable group by offer_id) as statusgroup
+group by offer_id
