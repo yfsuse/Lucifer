f651db10a5c5afd81eed35fb7444118ea3004878
Jeff <yfsuse@gmail.com>
2014-11-23 19:59:16 +0800

    fix some bugs:
    get last hour file use datetime module instead of use list
    
    modify url length from 255 to 1024
    
    add query field: status in sql at statusCrontab.py

--- Integration.py
+++ Integration.py
@@ -55,18 +55,22 @@ class Integration(object):
     def insertMysql(self):
         conn = MySQLdb.connect(host="10.1.5.60", user="django", passwd="django", db="statusdb", charset="utf8")
         """
-        <1> create database statusdb;
+        <1> create database statusdb default character set gbk;
         <2> use statusdb;
-        <3> create table statustable(conv_time datetime,
-                                     offer_id varchar(20),
-                                     status varchar(10),
-                                     url varchar(255));
+        <3> CREATE TABLE `statustable` (
+                `statusid` int(11) NOT NULL AUTO_INCREMENT,
+                `conv_time` datetime DEFAULT NULL,
+                `offer_id` varchar(20) DEFAULT NULL,
+                `status` varchar(10) DEFAULT NULL,
+                `url` varchar(1024) DEFAULT NULL,
+                PRIMARY KEY (`statusid`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=gbk
         <4> grant select,insert,update,delete,create,drop on statusdb.* to django@'%' identified by 'django';
         """
         cursor = conn.cursor()
         for record in self.jsonList:
             sql = "insert into statustable(conv_time, offer_id, status, url) values(%s, %s, %s, %s)"
-            result = cursor.execute(sql, record)
+            cursor.execute(sql, record)
         conn.commit()
         conn.close()
 
