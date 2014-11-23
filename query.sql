0492dff070589e748c4e3e0f56fac7531eff67e6
Jeff <yfsuse@gmail.com>
2014-11-23 20:00:15 +0800

    fix some bugs:
    get last hour file use datetime module instead of use list
    
    modify url length from 255 to 1024
    
    add query field: status in sql at statusCrontab.py

--- query.sql
+++ query.sql
@@ -4,5 +4,5 @@ MAX(CASE status WHEN 'Confirmed' THEN countstatus ELSE 0 END ) Confirmed,
 MAX(CASE status WHEN 'Accepted' THEN countstatus ELSE 0 END ) Accepted,
 (MAX(CASE status WHEN 'Confirmed' THEN countstatus ELSE 0 END )-MAX(CASE status WHEN 'Accepted' THEN countstatus ELSE 0 END)) diff,
 url from
-(select conv_time,offer_id, status, count(status) as countstatus, url from statustable group by offer_id) as statusgroup
+(select conv_time,offer_id, status, count(status) as countstatus, url from statustable group by offer_id, status) as statusgroup
 group by offer_id
